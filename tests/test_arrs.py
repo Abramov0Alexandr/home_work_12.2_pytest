abb2bf16bbe1dcbdb346438abd4c0f2c2538e0d3
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-02-18 20:40:44 +0300

    Довел прохождение тестов до 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,18 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
+def test_slice_normal():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+def test_slice_empty():
+    assert arrs.my_slice([]) == []
+
+
+def test_slice_negative_index():
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+
+
+def test_slice_negative_end():
+    assert arrs.my_slice([1, 2, 3], -4, -2) == [1]
